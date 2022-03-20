9bf5f38edb1becdedd167cae3bdd559701809ce1
Careaga2000 <careaga.snow@gmail.com>
2022-03-20 14:53:56 -0600

    Creacion de screens

---
+++ animeworld/app/screens/TopAnimes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from "react-native"
+
+export default function TopAnimes(){
+    return(
+        <View>
+            <Text>Top 10 Animes</Text>
+        </View>
+    )
+}
